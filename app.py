73f55837bac272798b1a892754200f3a35bda9b2
mymac@Kristo-Macbook.local <rahmanto@eurecom.fr>
2025-01-15 21:21:27 +0100

    edit to one single app.py file

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
-import integral
+import math
 
 app = Flask(__name__)
 
+def numerical_integration(lower, upper, N):
+    width = (upper - lower) / N
+    total_area = 0.0
+    for i in range(N):
+        x = lower + i * width
+        height = abs(math.sin(x))
+        total_area += height * width
+    return total_area
+
+def looper(lower: float, upper: float) -> list:
+    results = []
+    for N in [1, 10, 100, 100, 1000, 10000, 100000, 1000000]:
+        results.append(numerical_integration(lower, upper, N))
+    return results
 
 @app.route('/integralapp/<lower>/<upper>')
 def show_user_profile(lower, upper):
-    result = integral.looper(float(lower), float(upper))
+    result = looper(float(lower), float(upper))
     return f'User {result}'
